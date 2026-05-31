f8921a43938bb86b01e44486eef94de9ac07f2fe
Clifford Djan <2425400968@live.gctu.edu.gh>
2026-05-31 17:18:15 +0000

    initial commit

---
+++ initials.py
@@ -0,0 +1,39 @@
+
+#File: initials.py
+#Description:Print out my initials in large stylized letters.
+#Assignment number:1
+#
+#Name: Clifford Djan
+#STUDENT ID: 2425400968
+#Email: 2425400968@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor,Clifford Djan, this programming assignment is my own work
+# and i have not provided this code to any other student.
+
+
+def main():
+    #print my initials in large stylized letters
+
+    print()
+
+    print("...MCD")
+
+    print()
+
+
+print("...MMMM....MMMM........CCCCCCCCCCCC........DDDDDDDD....")
+print("...MMMM....MMMM........CCCC................DDDDDDDDDD..")
+print("...MM.MM..MM.MM........CC..................DD.......DDD")
+print("...MM.MM..MM.MM........CC..................DD........DD")
+print("...MM.MM..MM.MM........CC..................DD........DD")
+print("...MM..MMMM..MM........CC..................DD........DD")
+print("...MM..MMMM..MM........CC..................DD........DD")
+print("...MM...MM...MM........CC..................DD.......DDD")
+print("...MM...MM...MM...**...CCCC...........**...DDDDDDDDDD..")
+print("...MM...MM...MM...**...CCCCCCCCCCCC...**...DDDDDDDD....")
+
+print()
+
+
+
